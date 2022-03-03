8aa76a135b6a808338db141ebcfeecf35b645bfe
cc <cc_xie@gmail.com>
2022-03-03 18:10:47 +0800

    123

--- app/src/main/java/com/example/boomguess/MainActivity.kt
+++ app/src/main/java/com/example/boomguess/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.boomguess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.boomguess.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityMainBinding
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //  setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fun inputnum(view: View) {
+            val num = binding
+        }
+
     }
 }
