48674df725a83ab0fad10533a21fc516a8c8a0b7
cc <cc_xie@gmail.com>
2022-03-04 09:48:48 +0800

    可以顯示輸入的數字

--- app/src/main/java/com/example/boomguess/MainActivity.kt
+++ app/src/main/java/com/example/boomguess/MainActivity.kt
@@ -2,21 +2,28 @@ package com.example.boomguess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ContextThemeWrapper
 import android.view.View
+import android.widget.Toast
 import com.example.boomguess.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var binding: ActivityMainBinding
-        super.onCreate(savedInstanceState)
-        //  setContentView(R.layout.activity_main)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        fun inputnum(view: View) {
-            val num = binding
+    class MainActivity : AppCompatActivity() {
+        lateinit var binding: ActivityMainBinding //lateinit 晚點給
+        override fun onCreate(savedInstanceState: Bundle?) { //oncreate 一定會被執行
+            super.onCreate(savedInstanceState)
+            binding=ActivityMainBinding.inflate(layoutInflater)//宣告取得外觀中的變數
+            setContentView(binding.root)
         }
 
+
+
+        fun inputnum(view: View) {
+        val num=binding.guessnum.text.toString().toInt()
+        Log.d("you guess",num.toString())
+        Toast.makeText(this,num.toString(),Toast.LENGTH_LONG).show()//小訊息顯示較長時間的num值
     }
-}
+
+
+    }
+
